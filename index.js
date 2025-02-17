f1cf38765e9cae83cf1eede3b3a19db5a0affe40
Julian M <mifsudjulian007@gmail.com>
2025-02-17 12:58:57 +0100

    Removed half

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-console.log('Happy developing ✨')
+//Day 1
+/*
+const name = "John";
+console.log(name);
+var num = 1;//Can be accessed outside of block
+console.log(num);
+num = 3;
+console.log(num);
+
+let bo = false;
+console.log(bo);
+bo = true;
+console.log(bo);
+
+const some = undefined;
+console.log(typeof some);
+console.log(some);
+console.log(some ?? true);  //?
+console.log(some);
+
+const num1 = 2;
+let num2 = 3;
+function sum(a, b)  //Block of reusable code
+{
+  return a+b;
+}
+console.log(sum(num1, num2));
+
+const num = sum(10, 1);
+console.log(num);
+
+
+let x = 10;
+let y = 2;
+let subAns = (x, y) => x-y;
+
+console.log(subAns(x, y));
+
+let z = -1;
+let check = z => z >= 0
+console.log(check(z));
+
+let display = () => ("What's up");
+console.log(display());
+
+const array = [2,"HI"];
+array.push(false);
+array.push(["Bing", 0]);
+for(let i = 0; i < array.length; i++)
+{
+  console.log(array[i]);
+}
+*/
